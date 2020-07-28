5861c12b63e00ee86bfe42fcd200fc68e543954e
Naveen-HN <hnnaveen93@gmail.com>
2020-07-28 11:08:39 -0400

    MAde all changes

--- server.js
+++ server.js
@@ -11,16 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 // setup database
 
 const uri = process.env.MONGOLAB_URI;
 mongoose.connect(
-  uri,
-  {
+  uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
